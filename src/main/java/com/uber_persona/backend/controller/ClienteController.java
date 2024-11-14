4efa2d9c09cea606150bf4e73f0d1159f499d8b3
Victor Javier Vivas <victorjaviervivas@gmail.com>
2024-11-14 15:44:22 -0300

    Metodo listar cliente

--- src/main/java/com/uber_persona/backend/controller/ClienteController.java
+++ src/main/java/com/uber_persona/backend/controller/ClienteController.java
@@ -6,11 +6,14 @@ import com.uber_persona.backend.service.ClienteService;
 import com.uber_persona.backend.util.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.hibernate.MappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -25,6 +28,19 @@ public class ClienteController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ApiResponse<>("Cliente Creado", HttpStatus.CREATED.value(), toClienteSalida));
+    }
+    @GetMapping("/listar")
+    public ResponseEntity<ApiResponse<List<ToClienteSalida>>> listarClientes() {
+        try{
+            List<ToClienteSalida>toClienteSalidas=clienteService.listarClientes();
+            ApiResponse<List<ToClienteSalida>> response=
+                    new ApiResponse<>("Lista de clientes exitosa",HttpStatus.OK.value(),toClienteSalidas );
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+
+        }catch (MappingException e){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR.value(), null));
+        }
     }
 }
 
