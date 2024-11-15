367b8e2ff55d195bdf74fb25c6402ba1de69396d
Victor Javier Vivas <victorjaviervivas@gmail.com>
2024-11-15 11:31:30 -0300

    Interface cliente controller

--- src/main/java/com/uber_persona/backend/controller/ClienteController.java
+++ src/main/java/com/uber_persona/backend/controller/ClienteController.java
@@ -4,8 +4,10 @@ import com.uber_persona.backend.dto.entrada.ToClienteEntrada;
 import com.uber_persona.backend.dto.modificar.ToClienteModificar;
 import com.uber_persona.backend.dto.salida.ToClienteSalida;
 import com.uber_persona.backend.exception.ResourceNotFoundException;
+import com.uber_persona.backend.interfaces.IClienteController;
 import com.uber_persona.backend.service.ClienteService;
 import com.uber_persona.backend.util.ApiResponse;
+import com.uber_persona.backend.util.Va;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,48 +20,40 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/cliente")
-public class ClienteController {
+public class ClienteController implements IClienteController {
     private final ClienteService clienteService;
 
     @PostMapping("/crear")
-    public ResponseEntity<?> crearCliente(@RequestBody @Valid ToClienteEntrada toClienteEntrada) {
+    public ResponseEntity<ApiResponse<ToClienteSalida>> crearCliente(@RequestBody @Valid ToClienteEntrada toClienteEntrada) {
         ToClienteSalida toClienteSalida = clienteService.crearCliente(toClienteEntrada);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(new ApiResponse<>("Cliente Creado", HttpStatus.CREATED.value(), toClienteSalida));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(Va.CLIENTE_CREADO, HttpStatus.CREATED.value(), toClienteSalida));
     }
 
     @GetMapping("/listar")
     public ResponseEntity<ApiResponse<List<ToClienteSalida>>> listarClientes() {
         List<ToClienteSalida> toClienteSalidas = clienteService.listarClientes();
-        ApiResponse<List<ToClienteSalida>> response =
-                new ApiResponse<>("Lista de clientes exitosa", HttpStatus.OK.value(), toClienteSalidas);
+        ApiResponse<List<ToClienteSalida>> response = new ApiResponse<>(Va.LISTA_CLIENTE, HttpStatus.OK.value(), toClienteSalidas);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/buscar/{idCliente}")
     public ResponseEntity<ApiResponse<ToClienteSalida>> buscarClienteID(@PathVariable Long idCliente) throws ResourceNotFoundException {
         ToClienteSalida toClienteSalida = clienteService.obtenerClientePorId(idCliente);
-        ApiResponse<ToClienteSalida> response = new ApiResponse<>(
-                "Cliente encontrado", HttpStatus.OK.value(), toClienteSalida);
+        ApiResponse<ToClienteSalida> response = new ApiResponse<>(Va.CLIENTE_ENCONTRADO, HttpStatus.OK.value(), toClienteSalida);
         return ResponseEntity.ok(response);
     }
 
     @PutMapping("/modificar")
     public ResponseEntity<ApiResponse<ToClienteSalida>> modificarCLiente(@Valid @RequestBody ToClienteModificar toClienteModificar) throws ResourceNotFoundException {
         ToClienteSalida toClienteSalida = clienteService.actualizarCliente(toClienteModificar);
-        ApiResponse<ToClienteSalida> response = new ApiResponse<>(
-                "Cliente modificado", HttpStatus.OK.value(), toClienteSalida);
+        ApiResponse<ToClienteSalida> response = new ApiResponse<>(Va.CLIENTE_MODIFICADO, HttpStatus.OK.value(), toClienteSalida);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/eliminar/{idCliente}")
     public ResponseEntity<ApiResponse<Long>> eliminarCliente(@PathVariable Long idCliente) throws ResourceNotFoundException {
         clienteService.eliminarCliente(idCliente);
-        ApiResponse<Long> response = new ApiResponse<>("Cliente eliminado", HttpStatus.OK.value(), idCliente);
+        ApiResponse<Long> response = new ApiResponse<>(Va.CLIENTE_ELIMINADO, HttpStatus.OK.value(), idCliente);
         return ResponseEntity.ok(response);
     }
-
 }
-
-
