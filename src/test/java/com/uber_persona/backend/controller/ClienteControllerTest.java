f0163d07f527d49bde73abbb766607818a2dd22b
Victor Javier Vivas <victorjaviervivas@gmail.com>
2024-12-04 15:26:46 -0300

    Test Cliente Controller buscar por id

--- src/test/java/com/uber_persona/backend/controller/ClienteControllerTest.java
+++ src/test/java/com/uber_persona/backend/controller/ClienteControllerTest.java
@@ -2,6 +2,7 @@ package com.uber_persona.backend.controller;
 
 import com.uber_persona.backend.dto.entrada.ToClienteEntrada;
 import com.uber_persona.backend.dto.salida.ToClienteSalida;
+import com.uber_persona.backend.exception.ResourceNotFoundException;
 import com.uber_persona.backend.service.ClienteService;
 import com.uber_persona.backend.util.ApiResponse;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -23,17 +27,46 @@ class ClienteControllerTest {
     private ClienteController clienteController;
 
     @Test
-    void testCrearCliente(){
+    void testCrearCliente() {
         ToClienteEntrada toClienteEntrada = ToClienteEntrada.builder().build();
         ToClienteSalida toClienteSalida = ToClienteSalida.builder().build();
         when(clienteService.crearCliente(toClienteEntrada)).thenReturn(toClienteSalida);
 
-
-        ResponseEntity<ApiResponse<ToClienteSalida>> response = clienteController.crearCliente(toClienteEntrada);
+        ResponseEntity<ApiResponse<ToClienteSalida>> response = clienteController
+                .crearCliente(toClienteEntrada);
         assertNotNull(response);
-        assertEquals(201,response.getStatusCode().value());
-        assertSame(toClienteSalida,response.getBody().getData());
+        assertEquals(201, response.getStatusCode().value());
+        assertSame(toClienteSalida, response.getBody().getData());
     }
 
+    @Test
+    void testListarCliente() {
+        List<ToClienteSalida> clienteSalidas = Collections.emptyList();
+
+        ToClienteSalida toClienteSalida = ToClienteSalida.builder().build();
+        when(clienteService.listarClientes()).thenReturn(clienteSalidas);
+        ResponseEntity<ApiResponse<List<ToClienteSalida>>> responseClientes = clienteController
+                .listarClientes();
+        assertNotNull(responseClientes);
+        assertEquals(200, responseClientes.getStatusCode().value());
+        assertEquals(clienteSalidas, responseClientes.getBody().getData());
+        assertEquals("Lista de clientes exitosa", responseClientes.getBody().getMessage());
+        assertTrue(responseClientes.getBody().getData().isEmpty());
+    }
+
+    @Test
+    void testClienteBuscarId() throws ResourceNotFoundException {
+
+        ToClienteSalida toClienteSalida = ToClienteSalida.builder().build();
+
+        when(clienteService.obtenerClientePorId(null)).thenReturn(toClienteSalida);
+
+        ResponseEntity<ApiResponse<ToClienteSalida>> buscarClienteId = clienteController
+                .buscarClienteID(null);
+
+        assertNotNull(buscarClienteId);
+        assertEquals(200, buscarClienteId.getStatusCode().value());
+        assertSame(toClienteSalida, buscarClienteId.getBody().getData());
+    }
 
 }
